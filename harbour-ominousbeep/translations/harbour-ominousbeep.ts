92a88a218ee322e65ec00b9103c77554fbba7d72
llelectronics <leszek.lesner@web.de>
2020-04-17 07:48:04 +0200

    Add translations

--- harbour-ominousbeep/translations/harbour-ominousbeep.ts
+++ harbour-ominousbeep/translations/harbour-ominousbeep.ts
@@ -1,36 +1,29 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.0">
+<TS version="2.1">
 <context>
     <name>CoverPage</name>
     <message>
-        <source>My Cover</source>
-        <translation type="unfinished"></translation>
-    </message>
-</context>
-<context>
-    <name>FirstPage</name>
-    <message>
-        <source>Show Page 2</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>UI Template</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>Hello Sailors</source>
+        <source>Ominousbeep App</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
     <name>SecondPage</name>
     <message>
-        <source>Nested Page</source>
+        <source>About</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <source>Item</source>
+        <source>Inspired by Rick &amp; Morty TV series</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Copyright Leszek Lesner (C) 2020</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>License: GPLv3+</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
